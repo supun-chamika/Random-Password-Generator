a78042a2556bfdef8a35a6770f855fdf6d3206a3
supun-chamika <101165810+supun-chamika@users.noreply.github.com>
2022-03-08 23:32:49 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+# import models
+import random
+import string
+import os
+
+# get length of the password
+length = int(input("Enter the length of the password: "))
+
+# define characters
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+digit = string.digits
+symbol = string.punctuation
+
+# combine characters
+collection = lower + upper + digit + symbol
+
+# create list of characters
+temp = random.sample(collection, length)
+
+# create password
+password = "".join(temp)
+
+# show the generated password
+print("Your Password is: ", password)
+
+# get response from user to save the password
+save = input('Do you want to save the password(yes/no): ')
+
+# get name of the password
+password_name = input('Enter the name of the password: ')
+
+# saving password in txt file
+if save == 'yes' or 'y' or 'Y':
+    f = open(r"password.txt", "a")
+    f.write(password_name+': '+password + '\n')
+    f.close()
+
+# get the current directory
+directory = os.getcwd()
+
+# print txt file directory
+print('password saved in: \n' + directory + '\password.txt')
